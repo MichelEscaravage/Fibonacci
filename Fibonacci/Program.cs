5fe0b54823f170b41bd520bf0c1c0030c94cb2b1
Michel Escaravage <108881977+MichelEscaravage@users.noreply.github.com>
2024-10-07 15:34:50 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int firstDigit = -1;
+int secondDigit = 1;
+int result;
+
+for (int i = 0; i <= 20; i++)
+{
+
+    result = firstDigit + secondDigit;
+    firstDigit = secondDigit;
+    secondDigit = result;
+
+    Console.WriteLine(result);
+}
